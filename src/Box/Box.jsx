c7cf61a2601b135cce67e28a7a3020d575c9b05d
Amanda Stern <amanda.stern.73@gmail.com>
2023-02-21 16:40:27 -0500

    refactor box and box display to move state to boxdisplaycomponent

--- src/Box/Box.jsx
+++ src/Box/Box.jsx
@@ -1,24 +1,11 @@
-import { useState } from "react";
 import style from './Box.module.css'
 
 const Box = (props) => {
 
-  const {colors} = props
-
-  let randomColor = colors[Math.floor(Math.random() * colors.length)]
-  
-  const [color, setColor] = useState(randomColor)
-
-  function chooseColor(){
-    let newColor = colors[Math.floor(Math.random() * colors.length)]
-    do {
-      newColor = colors[Math.floor(Math.random() * colors.length)]
-    } while (newColor === color)
-    setColor(newColor)
-  }
+  const {handleClick, color} = props
   
   return ( 
-      <div onClick={() => chooseColor()} className={style.Box} style={{ backgroundColor: color }} />
+      <div onClick={() => handleClick()} className={style.Box} style={{ backgroundColor: color }} />
   );
 }
 
--- src/BoxDisplay/BoxDisplay.jsx
+++ src/BoxDisplay/BoxDisplay.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import Box from "../Box/Box";
 import style from './BoxDisplay.module.css'
 
 const BoxDisplay = ({ numBoxes }) => {
 
-  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'lilac', 'teal', 'pink', 'maroon', 'black']
+  
+  const [mangos, setMangos] = useState(
+    Array.from({length: numBoxes}).map(() => (
+      getRandomColor()
+    ))
+  )
+      
+  function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+  }
+      
+  function handleClick(boxIdx) {
+    const output = [...mangos].map((mango, idx) => {
+      let element
+      if (idx === boxIdx){
+        element = getRandomColor()
+      } else {
+        element = mango
+      }
+      return element
+    })
+    setMangos(output)
+  }
 
   return ( 
     <div className={style.boxContainer}>
-      {Array.from({length: numBoxes}).map((box)=>(
-        <Box colors={colors} key={'box-instance-' + JSON.stringify(box)} />
+      {mangos.map((box, idx)=>(
+        <Box key={'box-instance-' + (idx)} handleClick={() => handleClick(idx)} color={box} />
       ))}
     </div>
   );
